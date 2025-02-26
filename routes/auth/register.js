827ebe0b4d73fb7f2d6768139d3acc73d5c40b54
web-source-dev <muhammadnouman72321@gmail.com>
2025-02-26 22:34:18 +0500

    Update register.js

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const Log = require('../../models/Logs'); // Add this line to require the Logs model
 const sendEmail = require('../../utils/email');
