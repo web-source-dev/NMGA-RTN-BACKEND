3ee3a32bd5921426f3a21717dcaf8baa8ef6944e
RTNGLOBAL <risetonet@hotmail.com>
2025-04-26 16:15:41 +0800

    Remove unused Commitment model import from fetchAllDeals route

--- routes/Deals/fetchAllDeals.js
+++ routes/Deals/fetchAllDeals.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Deal = require('../../models/Deals');
-const Commitment = require('../../models/Commitments');
 
 router.get('/', async (req, res) => {
   try {
